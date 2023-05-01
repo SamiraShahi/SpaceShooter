f7f8167d1cea71af9a20faf8aa6dafacc2dfa5ff
Samira <shahisamira03@gmail.com>
2023-05-01 20:40:04 +0545

    first commit

---
+++ src/Background.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Background extends JPanel implements ActionListener {
+    private final int DELAY = 30;  //kati bistarai move hunxa bhanera
+    private final Image backgImage = new ImageIcon("C:\\Users\\ACER\\Pictures\\Capture.PNG").getImage();
+    private int y1 = 0;  // current y position of 1 backimg
+    private int y2 = backgImage.getHeight(null);  // current y position of 2 back img
+
+    public Background() {
+        Timer timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //2 copies banauxa bich ma gap audaina
+        g.drawImage(backgImage, 0, y1, null);
+        g.drawImage(backgImage, 0, y2, null);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        y1++;
+        y2++;
+        // If the first copy has moved off the screen, move it to the bottom
+        if (y1 >= getHeight()) {
+            y1 = y2 - backgImage.getHeight(null);
+        }
+        // If the second copy has moved off the screen, move it to the bottom
+        if (y2 >= getHeight()) {
+            y2 = y1 - backgImage.getHeight(null);
+        }
+      
+        repaint();
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(" Background");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(1000, 800);
+        frame.add(new Background());
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
