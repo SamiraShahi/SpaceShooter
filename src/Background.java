57a4bd435ab438c83d4c7d1e9cdc9d8c0c8d2574
Samira <shahisamira03@gmail.com>
2023-05-03 22:57:29 +0545

    commit

--- src/Background.java
+++ src/Background.java
@@ -1,56 +1,11 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-public class Background extends JPanel implements ActionListener {
-    private final int DELAY = 30;  //kati bistarai move hunxa bhanera
-    private final Image backgImage = new ImageIcon("C:\\Java All\\SpaceShooter\\Resources\\SpaceShooter-Background.jpg").getImage();
-
-
-    private int y1 = 0;  // current y position of 1 backing
-    private int y2 = backgImage.getHeight(null);  // current y position of 2 back img
-
-
-
-
-    public Background() {
-
-         setPreferredSize(new Dimension(50, 50));
-
-        Timer timer = new Timer(DELAY, this);
-        timer.start();
-
-
-
-    }
-
+public class Background extends JPanel {
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        //2 copies banauxa bich ma gap audaina
-        g.drawImage(backgImage, 0, y1, null);
-        g.drawImage(backgImage, 0, y2, null);
-
-
+        // Set the color to black
+        g.setColor(Color.BLACK);
+        // Fill the entire panel with black
+        g.fillRect(0, 0, getWidth(), getHeight());
     }
-
-    public void actionPerformed(ActionEvent e) {
-
-        y1++;
-        y2++;
-        // If the first copy has moved off the screen, move it to the bottom
-        if (y1 >= getHeight()) {
-            y1 = y2 - backgImage.getHeight(null);
-        }
-        // If the second copy has moved off the screen, move it to the bottom
-        if (y2 >= getHeight()) {
-            y2 = y1 - backgImage.getHeight(null);
-        }
-
-        repaint();
-    }
-
-
-
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
 
-public class Main {
+public class Main  {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame(" Space Shooter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
 
         Background background = new Background();
-        Spaceship spaceship = new Spaceship();
-
-
-        frame.setSize(800, 800);
         frame.add(background);
+        Spaceship spaceship = new Spaceship();
         frame.add(spaceship);
+        Stars star=new Stars();
+        frame.add(star);
+
+
 
 
 
@@ -22,10 +25,6 @@ public class Main {
         frame.setVisible(true);
         frame.setResizable(false);
 
-
-        
     }
-
-
 
 }
